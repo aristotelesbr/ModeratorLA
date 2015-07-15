5c82abc24c3d43c424b79ef4a6aa4f745a78e7b4
Aristóteles <aristotelesbr2014@gmail.com>
2015-07-15 14:21:35 -0300

    Created CRUD for Expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,49 @@
 class ExpensesController < ApplicationController
+  before_action :find_expense, only: [:show, :edit, :update, :destroy]
+
   def index
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def show
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+
+    if @expense.save
+      redirect_to @expense, notice: 'notice.expense.success'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @expense.update(expense_params)
+      redirect_to @expense, notice: 'notice.expense.update'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    redirect_to root_path, alert: 'alert.expense.destroy'
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:value, :description, :card)
+  end
+
+  def find_expense
+    @expense = Expense.find(params[:id])
   end
 end
