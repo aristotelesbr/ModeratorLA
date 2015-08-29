39d68481f8efb24100ea2ed0c0a7614ba86885a5
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2015-08-29 17:02:12 -0300

    Removed column card from expenses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829111025) do
+ActiveRecord::Schema.define(version: 20150829195928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20150829111025) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.integer  "quantity"
-    t.string   "card"
   end
 
   add_index "expenses", ["user_id"], name: "index_expenses_on_user_id", using: :btree
